da3e0ccef1dc47c043ae4bcdfca11fe08b2564d9
Scott Kennedy <scottyzen@gmail.com>
2020-03-22 19:19:47 +0000

    Added blog

---
+++ pages/blog/_blog.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <article class="fadeUp">
+      <h1>{{blogPost.title}}</h1>
+      <div>{{blogPost.body}}</div>
+    </article>
+  </div>
+</template>
+
+
+<script>
+export default {
+  async asyncData({ params, payload }) {
+    if (payload) return { blogPost: payload }
+    else
+      return {
+        blogPost: await require(`~/assets/content/blog/${params.blog}.json`)
+      }
+  }
+}
+</script>
---
+++ pages/blog/index.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="container">
+    <ul>
+      <li class="fadeUp" v-for="(post, index) in blogPosts" :key="post.id">
+        <nuxt-link
+          :to="`/blog/${post.slug}`"
+          class="fadeUp animation-duration-1"
+          :class="`delay-${index*100}`"
+        >{{post.title}}</nuxt-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    blogPosts() {
+      return this.$store.state.blogPosts
+    }
+  }
+}
+</script>
+
+<style>
+.fadeUp {
+  animation: 1s appear forwards;
+  opacity: 0;
+  transform: translateY(10px);
+}
+
+@keyframes appear {
+  100% {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
+.animation-duration-1 {
+  animation-duration: 600ms;
+}
+.delay-0 {
+  animation-delay: 0;
+}
+.delay-100 {
+  animation-delay: 200ms;
+}
+.delay-200 {
+  animation-delay: 400ms;
+}
+.delay-300 {
+  animation-delay: 600ms;
+}
+.delay-400 {
+  animation-delay: 800ms;
+}
+.delay-500 {
+  animation-delay: 1000ms;
+}
+</style>
