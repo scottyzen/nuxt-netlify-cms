a6dba453cdaa19a132781c5c3866dafd275717bc
Scott Kennedy <scottyzen@gmail.com>
2020-04-04 14:53:32 +0100

    Styled form

--- pages/index.vue
+++ pages/index.vue
@@ -108,7 +108,7 @@ export default {
           .then((res) => {
             console.log(res)
 
-            if (res.data.status.status === 'succeeded') {
+            if (res.data.status === 'succeeded') {
               this.successMessage = `Your order was successful.`
             }
           })
