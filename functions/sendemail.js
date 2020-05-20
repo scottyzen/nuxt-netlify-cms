5a8383c6a2fcd8ee09e0d05733c08ff5216404f2
Scott Kennedy <scottyzen@gmail.com>
2020-05-20 23:14:51 +0100

    sendgrid

--- functions/sendemail.js
+++ functions/sendemail.js
@@ -1,5 +1,17 @@
+require("dotenv").config()
+
+const statusCode = 200;
+const headers = {
+  "Access-Control-Allow-Origin" : "*",
+  "Access-Control-Allow-Headers": "Content-Type"
+};
+const sgMail = require('@sendgrid/mail');
+
 exports.handler = function(event, context, callback) {
-    const sgMail = require('@sendgrid/mail');
+
+    console.log('Here comes te magic');
+
+    
     sgMail.setApiKey(process.env.SENDGRID_API_KEY);
     const msg = {
       to: 'scottyzen@gmail.com',
@@ -9,4 +21,10 @@ exports.handler = function(event, context, callback) {
       html: '<strong>and easy to do anywhere, even with Node.js</strong>',
     };
     sgMail.send(msg);
+
+    callback(null, {
+        statusCode,
+        headers,
+        body: JSON.stringify({msg})
+      });
 };
