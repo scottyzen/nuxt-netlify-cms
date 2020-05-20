f2724654eb7a3b20c1655d0364e55ca02990e5f9
Scott Kennedy <scottyzen@gmail.com>
2020-05-20 23:38:23 +0100

    sendgrid

--- functions/sendemail.js
+++ functions/sendemail.js
@@ -1,28 +1,28 @@
 require("dotenv").config()
 
-const statusCode = 200;
-const headers = {
-  "Access-Control-Allow-Origin" : "*",
-  "Access-Control-Allow-Headers": "Content-Type"
-};
-exports.handler = function(event, context, callback) {
-    const sgMail = require('@sendgrid/mail');
+const sgMail = require('@sendgrid/mail')
 
-    console.log('Here comes te magic');
+exports.handler = function (event, context, callback) {
 
-    
-    sgMail.setApiKey('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');
-    const msg = {
-      to: 'scottyzen@gmail.com',
-      from: 'scottyzen@gmail.com',
-      subject: 'Sending with Twilio SendGrid is Fun',
-      text: 'and easy to do anywhere, even with Node.js',
-      html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-    };
-    sgMail.send(msg);
+  sgMail.setApiKey('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')
+  let body = JSON.parse(event.body)
 
-    callback(null, {
-        statusCode: 200,
-        body: "Hello, World!"
-      });
-};
+  let msg = {
+    to: 'scottyzen@gmail.com',
+    from: 'scottyzen@gmail.com',
+    subject: 'Website Contact Form',
+    text: 'Someone has filled out a form on your website, check out the message they left and get in touch with them :)',
+    templateId: 'd-39c15b77271449548e2f13b66920f808',
+    html: '<p>Hello HTML world!</p>'
+  }
+
+//   sgMail.send(msg);
+
+  sgMail.send(msg).then(() => {
+    console.log(`Contact form sent`)
+    callback()
+  }).catch(e => {
+    console.error(e.toString())
+    callback(e.toString())
+  })
+}
