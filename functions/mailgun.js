5dde8806b7d483c186f4ce2c3bf041e4b261fcde
Scott Kennedy <scottyzen@gmail.com>
2020-05-21 20:51:53 +0100

    Added mailgun

--- functions/mailgun.js
+++ functions/mailgun.js
@@ -7,13 +7,14 @@ const mailgun = require("mailgun-js");
 const DOMAIN = "sandbox3fe1f38680134983b035fe584981ec9d.mailgun.org";
 const mg = mailgun({apiKey: process.env.MAILGUN, domain: DOMAIN});
 const data = {
-	from: "Mailgun Sandbox <postmaster@sandbox3fe1f38680134983b035fe584981ec9d.mailgun.org>",
+	from: "Netlify Function Example <scott@loving-wing-0e7dc4.netlify.app>",
 	to: "scottyzen@me.com",
-	subject: "Hello",
+	subject: "Hello World",
 	text: "Testing some Mailgun awesomness!"
 };
 mg.messages().send(data, function (error, body) {
-    console.log(body);
+    console.log('body: ', body);
+    console.log('data: ', data);
     callback(null, {
         statusCode: 200,
         status: 'Successfull',
